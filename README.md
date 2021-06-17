b2f307860a487f95ea1eba0603befd772608ce74
tintinrevient <zhaobenben007@googlemail.com>
2021-06-17 14:06:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Pose CycleGAN
+
+## Dataset
+
+Download the chosen dataset:
+* apple2orange
+* horse2zebra (buggy: some images don't have the RGB channel)
+```bash
+./download_dataset apple2orange
+```
+
+```bash
+├── datasets                   
+|   ├── <dataset_name>         # i.e. apple2orange
+|   |   ├── train              # Training
+|   |   |   ├── A              # Contains domain A images (i.e., Apple)
+|   |   |   └── B              # Contains domain B images (i.e., Orange)
+|   |   └── test               # Testing
+|   |   |   ├── A              # Contains domain A images (i.e., Apple)
+|   |   |   └── B              # Contains domain B images (i.e., Orange)
+```
+
+## Training
+
+```bash
+python train.py --dataset apple2orange --cuda --n_epochs 20 --decay_epoch 10
+```
+
+## Teting
+
+```bash
+python test.py --dataset apple2orange --cuda
+```
+
---
+++ train.py
@@ -0,0 +1,223 @@
+import argparse
+import itertools
+import os.path
+
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+from PIL import Image
+import torch
+
+from models import Generator
+from models import Discriminator
+from utils import ReplayBuffer
+from utils import LambdaLR
+from utils import Logger
+from utils import weights_init_normal
+from datasets import ImageDataset
+
+from tqdm import tqdm
+import torchvision.utils as vutils
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--epoch', type=int, default=0, help='starting epoch')
+parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
+parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
+parser.add_argument('--dataset', type=str, default='horse2zebra', help='root directory of the dataset')
+parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate')
+parser.add_argument('--decay_epoch', type=int, default=100, help='epoch to start linearly decaying the learning rate to 0')
+parser.add_argument('--size', type=int, default=256, help='size of the data crop (squared assumed)')
+parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
+parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
+parser.add_argument('--cuda', action='store_true', help='use GPU computation')
+parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
+parser.add_argument('--print_freq', type=int, default=100, help='every how many images to save the sample images')
+
+opt = parser.parse_args()
+print(opt)
+
+if torch.cuda.is_available() and not opt.cuda:
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+###### Definition of variables ######
+# Networks
+netG_A2B = Generator(opt.input_nc, opt.output_nc)
+netG_B2A = Generator(opt.output_nc, opt.input_nc)
+netD_A = Discriminator(opt.input_nc)
+netD_B = Discriminator(opt.output_nc)
+
+if opt.cuda:
+    netG_A2B.cuda()
+    netG_B2A.cuda()
+    netD_A.cuda()
+    netD_B.cuda()
+
+netG_A2B.apply(weights_init_normal)
+netG_B2A.apply(weights_init_normal)
+netD_A.apply(weights_init_normal)
+netD_B.apply(weights_init_normal)
+
+# Lossess
+criterion_GAN = torch.nn.MSELoss()
+criterion_cycle = torch.nn.L1Loss()
+criterion_identity = torch.nn.L1Loss()
+
+# Optimizers & LR schedulers
+optimizer_G = torch.optim.Adam(itertools.chain(netG_A2B.parameters(), netG_B2A.parameters()),
+                                lr=opt.lr, betas=(0.5, 0.999))
+optimizer_D_A = torch.optim.Adam(netD_A.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+optimizer_D_B = torch.optim.Adam(netD_B.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+
+lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(optimizer_G, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
+lr_scheduler_D_A = torch.optim.lr_scheduler.LambdaLR(optimizer_D_A, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
+lr_scheduler_D_B = torch.optim.lr_scheduler.LambdaLR(optimizer_D_B, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
+
+# Inputs & targets memory allocation
+Tensor = torch.cuda.FloatTensor if opt.cuda else torch.Tensor
+input_A = Tensor(opt.batch_size, opt.input_nc, opt.size, opt.size)
+input_B = Tensor(opt.batch_size, opt.output_nc, opt.size, opt.size)
+target_real = Variable(Tensor(opt.batch_size).fill_(1.0), requires_grad=False)
+target_fake = Variable(Tensor(opt.batch_size).fill_(0.0), requires_grad=False)
+
+fake_A_buffer = ReplayBuffer()
+fake_B_buffer = ReplayBuffer()
+
+# Dataset loader
+transforms_ = [ transforms.Resize(int(opt.size*1.12), Image.BICUBIC), 
+                transforms.RandomCrop(opt.size), 
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)) ]
+dataloader = DataLoader(ImageDataset(os.path.join('datasets', opt.dataset), transforms_=transforms_, unaligned=True),
+                        batch_size=opt.batch_size, shuffle=True, num_workers=opt.n_cpu)
+
+# Loss plot
+# logger = Logger(opt.n_epochs, len(dataloader))
+###################################
+
+# Directories
+output_dir = os.path.join('output', opt.dataset)
+weights_dir = os.path.join('weights', opt.dataset)
+if not os.path.exists(output_dir):
+    os.makedirs(os.path.join(output_dir, 'A'))
+    os.makedirs(os.path.join(output_dir, 'B'))
+if not os.path.exists(weights_dir):
+    os.makedirs(weights_dir)
+
+###### Training ######
+for epoch in range(opt.epoch, opt.n_epochs):
+    progress_bar = tqdm(enumerate(dataloader), total=len(dataloader))
+    for i, data in progress_bar:
+        # Set model input
+        real_A = Variable(input_A.copy_(data['A']))
+        real_B = Variable(input_B.copy_(data['B']))
+
+        ###### Generators A2B and B2A ######
+        optimizer_G.zero_grad()
+
+        # Identity loss
+        # G_A2B(B) should equal B if real B is fed
+        same_B = netG_A2B(real_B)
+        loss_identity_B = criterion_identity(same_B, real_B)*5.0
+        # G_B2A(A) should equal A if real A is fed
+        same_A = netG_B2A(real_A)
+        loss_identity_A = criterion_identity(same_A, real_A)*5.0
+
+        # GAN loss
+        fake_B = netG_A2B(real_A)
+        pred_fake = netD_B(fake_B)
+        loss_GAN_A2B = criterion_GAN(pred_fake, target_real)
+
+        fake_A = netG_B2A(real_B)
+        pred_fake = netD_A(fake_A)
+        loss_GAN_B2A = criterion_GAN(pred_fake, target_real)
+
+        # Cycle loss
+        recovered_A = netG_B2A(fake_B)
+        loss_cycle_ABA = criterion_cycle(recovered_A, real_A)*10.0
+
+        recovered_B = netG_A2B(fake_A)
+        loss_cycle_BAB = criterion_cycle(recovered_B, real_B)*10.0
+
+        # Total loss
+        loss_G = loss_identity_A + loss_identity_B + loss_GAN_A2B + loss_GAN_B2A + loss_cycle_ABA + loss_cycle_BAB
+        loss_G.backward()
+        
+        optimizer_G.step()
+        ###################################
+
+        ###### Discriminator A ######
+        optimizer_D_A.zero_grad()
+
+        # Real loss
+        pred_real = netD_A(real_A)
+        loss_D_real = criterion_GAN(pred_real, target_real)
+
+        # Fake loss
+        fake_A = fake_A_buffer.push_and_pop(fake_A)
+        pred_fake = netD_A(fake_A.detach())
+        loss_D_fake = criterion_GAN(pred_fake, target_fake)
+
+        # Total loss
+        loss_D_A = (loss_D_real + loss_D_fake)*0.5
+        loss_D_A.backward()
+
+        optimizer_D_A.step()
+        ###################################
+
+        ###### Discriminator B ######
+        optimizer_D_B.zero_grad()
+
+        # Real loss
+        pred_real = netD_B(real_B)
+        loss_D_real = criterion_GAN(pred_real, target_real)
+        
+        # Fake loss
+        fake_B = fake_B_buffer.push_and_pop(fake_B)
+        pred_fake = netD_B(fake_B.detach())
+        loss_D_fake = criterion_GAN(pred_fake, target_fake)
+
+        # Total loss
+        loss_D_B = (loss_D_real + loss_D_fake)*0.5
+        loss_D_B.backward()
+
+        optimizer_D_B.step()
+        ###################################
+
+        progress_bar.set_description(
+            f"[{epoch}/{opt.n_epochs - 1}][{i}/{len(dataloader) - 1}] "
+            f"Loss_D: {(loss_D_A + loss_D_B).item():.4f} "
+            f"Loss_G: {loss_G.item():.4f} "
+            f"Loss_G_identity: {(loss_identity_A + loss_identity_B).item():.4f} "
+            f"loss_G_GAN: {(loss_GAN_A2B + loss_GAN_B2A).item():.4f} "
+            f"loss_G_cycle: {(loss_cycle_ABA + loss_cycle_BAB).item():.4f}")
+
+        if i % opt.print_freq == 0:
+            vutils.save_image(real_A,
+                              f"{output_dir}/A/real_sample_epoch_{epoch}.png",
+                              normalize=True)
+            vutils.save_image(real_B,
+                              f"{output_dir}/B/real_sample_epoch_{epoch}.png",
+                              normalize=True)
+
+            fake_image_A = 0.5 * (netG_B2A(real_B).data + 1.0)
+            fake_image_B = 0.5 * (netG_A2B(real_A).data + 1.0)
+
+            vutils.save_image(fake_image_A.detach(),
+                              f"{output_dir}/A/fake_sample_epoch_{epoch}.png",
+                              normalize=True)
+            vutils.save_image(fake_image_B.detach(),
+                              f"{output_dir}/B/fake_sample_epoch_{epoch}.png",
+                              normalize=True)
+
+    # Update learning rates
+    lr_scheduler_G.step()
+    lr_scheduler_D_A.step()
+    lr_scheduler_D_B.step()
+
+    # Save models checkpoints
+    torch.save(netG_A2B.state_dict(), os.path.join(weights_dir, 'netG_A2B.pth'))
+    torch.save(netG_B2A.state_dict(), os.path.join(weights_dir, 'netG_B2A.pth'))
+    torch.save(netD_A.state_dict(), os.path.join(weights_dir, 'netD_A.pth'))
+    torch.save(netD_B.state_dict(), os.path.join(weights_dir, 'netD_B.pth'))
+###################################
