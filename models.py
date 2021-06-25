4b9559352c5439b147e0ee7bf5f715eec091068d
Zhao Shu <zhaobenben007@googlemail.com>
2021-06-25 13:14:22 +0200

    update

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+from utils import weights_init_normal
 
 
 class ResidualBlock(nn.Module):
@@ -116,3 +119,62 @@ class PatchDiscriminator(Discriminator):
         input = input.permute(0, 2, 4, 1, 3, 5).contiguous().view(B * Y * X, C, size, size)
 
         return super().forward(input)
+
+# Patch MLP
+# Potential issues: currently, we use the same patch_ids for multiple images in the batch
+class PatchMLP(nn.Module):
+
+    def __init__(self, nc=256):
+        super(PatchMLP, self).__init__()
+        self.nc = nc  # hard-coded
+        self.mlp_init = False
+
+    def create_mlp(self, feats):
+        for mlp_id, feat in enumerate(feats):
+            input_nc = feat.shape[1]
+            mlp = nn.Sequential(*[nn.Linear(input_nc, self.nc), nn.ReLU(), nn.Linear(self.nc, self.nc)])
+            mlp.cuda()
+            mlp.apply(weights_init_normal)
+
+            setattr(self, 'mlp_%d' % mlp_id, mlp)
+
+        self.mlp_init = True
+
+    def forward(self, feats, num_patches=64, patch_ids=None):
+        return_ids = []
+        return_feats = []
+
+        if not self.mlp_init:
+            self.create_mlp(feats)
+
+        for feat_id, feat in enumerate(feats):
+            B, H, W = feat.shape[0], feat.shape[2], feat.shape[3]
+            feat_reshape = feat.permute(0, 2, 3, 1).flatten(1, 2)
+
+            if num_patches > 0:
+                if patch_ids is not None:
+                    patch_id = patch_ids[feat_id]
+                else:
+                    patch_id = torch.randperm(feat_reshape.shape[1], device=feats[0].device)
+                    patch_id = patch_id[:int(min(num_patches, patch_id.shape[0]))]  # .to(patch_ids.device)
+
+                x_sample = feat_reshape[:, patch_id, :].flatten(0, 1)  # reshape(-1, x.shape[1])
+
+            else:
+                x_sample = feat_reshape
+                patch_id = []
+
+            # return ids
+            return_ids.append(patch_id)
+
+            # return samples
+            mlp = getattr(self, 'mlp_%d' % feat_id)
+            x_sample = mlp(x_sample)
+            x_sample = x_sample.div(torch.norm(x_sample))
+
+            if num_patches == 0:
+                x_sample = x_sample.permute(0, 2, 1).reshape([B, x_sample.shape[-1], H, W])
+
+            return_feats.append(x_sample)
+
+        return return_feats, return_ids
